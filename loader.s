6ae342bb584c8f51ba74f72f277a98f0d825f7e1
Leandro Assis dos Santos <assissantosleandro@poli.ufrj.br>
2023-04-25 20:54:11 -0300

    inicialização do .bss

--- loader.s
+++ loader.s
@@ -4,15 +4,15 @@
 
 _start:
     la sp, _stack_start
-    lui a1, _bss_start
-    lui a2, _bss_end
+    la a1, _bss_start
+    la a2, _bss_end
     jal ra, InitializeBSS    
     call kernelMain
     
 InitializeBSS:
-    sd x0, 0(a1)
-    addi a1, a1, 8 # possivelmente fazer com instruções vetoriais
-    blt a1, a2, InitializeBSS
+    sd a1, 0(x0)
+    addi a1, a1, -8 # possivelmente fazer com instruções vetoriais
+    bgt a1, a2, InitializeBSS
     jalr x0, 0(ra)
 
 _hlt:
